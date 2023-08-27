e21d15a7abd97ba86e2348af1d1ea20612a5b75f
janaep <janaeperez117@gmail.com>
2023-08-27 17:21:49 -0400

    Push

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({ todos, toggleTodo }) {
   return (
-    <div>
-      Hello World
-      {/* <h1>Hello World</h1> */}
-    </div>
+    todos.map(todo => {
+      return <Todo key={todo.id} toggleTodo = {toggleTodo} todo = {todo} />
+    })
   )
 }
